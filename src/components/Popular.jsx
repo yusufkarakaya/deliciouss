667e70619a3510c458ec5a0c05716bf451040769
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-04-12 17:51:39 -0700

    text align fixed

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -61,23 +61,34 @@ const Wrapper = styled.div`
 `;
 
 const Card = styled.div`
+  min-height: 20rem;
   border-radius: 2rem;
   position: relative;
+  overflow: hidden;
 
   img {
-    position: relative;
-    width: 100%;
-    height: 18rem;
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
   }
   p {
     position: absolute;
-    bottom: 1.5rem;
-    color: white;
-    font-weight: 600;
     z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
     text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -54,46 +54,46 @@ function Veggie() {
 }
 
 const Wrapper = styled.div`
-  margin: 5rem 0rem;
-
-  h3 {
-    margin-bottom: 2rem;
-  }
+  margin: 4rem 0rem;
 `;
 
 const Card = styled.div`
+  min-height: 20rem;
   border-radius: 2rem;
+  overflow: hidden;
   position: relative;
 
   img {
-    position: relative;
-    width: 100%;
-    height: 18rem;
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
   }
   p {
     position: absolute;
-    bottom: 1.5rem;
-    color: white;
-    font-weight: 600;
     z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
     text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
-
 const Gradient = styled.div`
+  z-index: 3;
   position: absolute;
-  top: 0;
-  height: 100%;
   width: 100%;
-  background-image: linear-gradient(
-    to bottom,
-    rgba(0, 0, 0, 0),
-    rgba(0, 0, 0, 0.5)
-  );
-  z-index: 5;
-  border-radius: 2rem;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Veggie;
