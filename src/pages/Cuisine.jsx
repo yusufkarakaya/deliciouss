9a8d8ae9ed040f01086801bfc3d1694523a773cb
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-04-14 16:43:00 -0700

    created Cuisine page

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Cuisine() {
   let params = useParams();
   const [cuisine, setCuisine] = useState([]);
-
-  useEffect(() => {
-    getCuisine(params.type);
-  }, [params.type]);
 
   const getCuisine = async (type) => {
     const api = await fetch(
@@ -18,7 +15,36 @@ function Cuisine() {
     console.log(data.results);
   };
 
-  return <div>Cuisine</div>;
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        return (
+          <div>
+            <img src={item.image} alt={item.title} />
+            <p>{item.title}</p>
+          </div>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  margin-top: 3rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+
+  img {
+    border-radius: 2rem;
+  }
+  p {
+    text-align: center;
+  }
+`;
 
 export default Cuisine;
