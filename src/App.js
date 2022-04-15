5589b61dc30121dac20564f681ed9ebdb2f9cb0c
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-04-14 21:45:20 -0700

    logo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
+import { GiKnifeFork } from 'react-icons/gi';
 import Pages from './pages/Pages';
 import Category from './components/Category';
 import Search from './components/Search';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Logo to={'/'}>
+          <GiKnifeFork />
+          <p>Deliciouss</p>
+        </Logo>
         <Search />
         <Category />
         <Pages />
@@ -14,5 +21,24 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  margin: 3rem 0rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 2.2rem;
+  gap: 0.6rem;
+  text-decoration: none;
+  color: black;
+
+  p {
+    font-size: 1.5rem;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 export default App;
